1c72a2ecc1a3bd99e9f8cbbeb39f871d82939a14
etla venkateshwarlu <vte558@gmail.com>
2026-09-21 10:49:16 +0000

    store query chnanges added

--- analyses/stores_analyses.sql
+++ analyses/stores_analyses.sql
@@ -9,3 +9,14 @@ SELECT
 FROM `vctbatch-9916-500703.retail_raw.INFORMATION_SCHEMA.COLUMNS`
 WHERE table_name = 'stores'
 ORDER BY ordinal_position
+
+
+
+select 
+    store_id,
+    store_name,
+    city,
+    state,
+     from 
+
+{{source("retail_raw","stores")}}
